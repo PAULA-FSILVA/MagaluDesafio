855351d8aab1b55204171ade7c2f14b386748c24
Nádia <nadia.aguiar@hotmail.com>
2020-11-17 01:53:23 +0100

    Update views.py
    commitando os views

--- products/views.py
+++ products/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.http import Http404
+from .models import Product
+from django.http import JsonResponse
 
 def index(request):
-    return HttpResponse("Olá mundo! Você está no Products")
+    models = Product.objects.all().order_by('title')
+
+    products = []
+    for model in models:
+        products.append(model.to_dict())
+
+    output = {"products": products}
+
+    return JsonResponse(output)
+
+
+def detail(request, id_product):
+    try:
+        product = Product.objects.get(pk = id_product)
+    except Product.DoesNotExist:
+        raise Http404 ("Produto inativo ou em falta. ")
+
+    return JsonResponse(product.to_dict())
