627a1fd2745e8bd4237fc397eab4568aba9f4c0f
Nádia <nadia.aguiar@hotmail.com>
2020-11-17 01:50:32 +0100

    Update models.py
    commitando a classe do Seller e do Produto

--- products/models.py
+++ products/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Seller(models.Model):
+    name = models.CharField(max_length=200)
+    seller_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+status_product = [
+    ("A", "Active"),
+    ("I", "Inactive"),
+]
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    price = models.FloatField()
+    id_product = models.IntegerField()
+    seller = models.ForeignKey(Seller, null=True,on_delete=models.SET_NULL)
+    qt_stock = models.IntegerField()
+    status = models.CharField(max_length=1, choices=status_product)
+
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "price": self.price,
+            "id_product": self.id_product,
+            "seller": self.seller.name,
+            "qt_stock": self.qt_stock,
+            "status": self.status
+        }
+
+
+
+    def __str__(self):
+        return self.title
