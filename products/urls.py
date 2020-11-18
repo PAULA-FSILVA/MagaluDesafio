36cc5c0586028e53f533a541a4c11cd78c97ea5e
PAULA-FSILVA <74570364+PAULA-FSILVA@users.noreply.github.com>
2020-11-17 21:17:52 -0300

    Adição do método de criação de produtos

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:id_product>/', views.detail, name="detail")
+    path('<int:id_product>/', views.detail, name="detail"),
+    path('create', views.create_product, name='create_product')
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.http import Http404
 from .models import Product
 from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     models = Product.objects.all().order_by('title')
@@ -18,6 +20,31 @@ def detail(request, id_product):
     try:
         product = Product.objects.get(pk = id_product)
     except Product.DoesNotExist:
-        raise Http404 ("Produto inativo ou em falta. ")
+        raise Http404 ("Produto inativo ou em falta.")
 
     return JsonResponse(product.to_dict())
+    
+
+@csrf_exempt
+def create_product (request):
+    payload = json.loads(request.body)
+    title = payload.get("title")
+    price = payload.get("price", 0)
+    seller = payload.get("seller")
+    qt_stock = payload.get("qt_stock")
+    status = payload.get("status")
+
+    #if not name:
+    #    return HttpResponseBadRequest("name is required")
+
+    product = Product()
+    product.title = title
+    product.price = price
+    product.seller = seller
+    product.qt_stock = qt_stock
+    product.status = status
+    product.save()
+
+    res = {"product": product.to_dict()}
+    return JsonResponse (res)
+
